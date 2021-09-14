d9885b683179745fa31f3a256e0a99b87d8aeb17
codjosoft <yeboah.determined.isaac@gmail.com>
2021-09-14 07:56:16 +0000

    update

--- index.js
+++ index.js
@@ -4,38 +4,54 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const exphbs = require('express-handlebars');
 
+const port = process.env.PORT || 4000;
+
+// body parser 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use(cors());
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// handlebars config
+app.set("view engine", "hbs")
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
 
 
 
-const port = process.env.PORT || 6000;
+// app.use(express.static(__dirname + '/public'));
 
-
-
+    const user = {
+        name : 'John',
+        age : '25',
+        job: 'dev'
+    }
+    const license = {
+        name: 'kojo',
+        age : 27,
+        location: 'Accra'
+    }
 // Landing page
 app.get('/', (req, res) => {
 
-    res.send('handle bars view engine begins')
+    // res.send('handle bars view engine begins')
+    res.render('main',{
+        title: 'Hanblebars',
+        user,
+        license 
+    })
     // res.sendFile(__dirname + '/public/index.html');
 
 })
 
-
-
-
-
-
-
-
-
-
-
-
+// start server
 app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
+    //   console.log(user)
 });
